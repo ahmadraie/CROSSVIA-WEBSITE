614e24481c1026c133877b94933a8caec7a59320
Muhye aldeen <86913045+MuhyeAlRaie@users.noreply.github.com>
2024-11-20 06:32:27 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,30 +17,6 @@ window.addEventListener("load", function () {
 
 
 
-const player = document.getElementById('snowboarding-player');
-const sections = document.querySelectorAll('.content-section');
-
-window.addEventListener('scroll', () => {
-  let sectionIndex = 0;
-
-  // Loop through each section to determine the current section in view
-  sections.forEach((section, index) => {
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.offsetHeight;
-    const scrollPosition = window.scrollY + window.innerHeight / 2;
-
-    if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-      sectionIndex = index;
-    }
-  });
-
-  // Alternate position and flip based on section index (right for even, left for odd)
-  const isRightSide = sectionIndex % 2 === 0;
-  const position = isRightSide ? '0vw' : '-50vw';
-  
-  // Set the position and flip the player image when on the right side
-  player.style.transform = `translateX(${position}) ${isRightSide ? 'scaleX(1)' : 'scaleX(-1)'}`;
-});
 
 // Optional: Disable Bootstrap's toggle functionality on hover-only dropdowns
 document.querySelector('#expertiseDropdown').addEventListener('click', function (e) {
